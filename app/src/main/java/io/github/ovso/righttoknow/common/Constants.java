2deeeea7cc3bce4085501796905e40f62adafc81
ohjaeho <ovsoce@gmail.com>
2017-08-15 15:09:15 +0900

    Edit Constants

--- app/src/main/java/io/github/ovso/righttoknow/common/Constants.java
+++ app/src/main/java/io/github/ovso/righttoknow/common/Constants.java
@@ -8,9 +8,9 @@ public class Constants {
   public final static String BASE_URL = "http://info.childcare.go.kr/info/cfvp";
   public final static String VIOLATION_FACILITY_PATH = "/VioltfcltySlL.jsp";
   public final static String VIOLATION_FACILITY_PARAMS = "?total=89&offset=0&limit=200";
-  public final static String URL_REVIEW = "market://details?id=kr.blogspot.ovsoce.hotkey";
+  public final static String URL_REVIEW = "market://details?id=io.github.ovso.righttoknow";
   public final static String URL_SHARE =
-      "https://play.google.com/store/apps/details?id=kr.blogspot.ovsoce.hotkey";
+      "https://play.google.com/store/apps/details?id=io.github.ovso.righttoknow";
   public final static int ITEM_VIOLATION_FACILITY = 0;
   public final static int ITEM_VIOLATOR = 1;
 
