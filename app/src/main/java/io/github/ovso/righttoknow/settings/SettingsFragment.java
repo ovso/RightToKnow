338b471a45fd07983ed916441d4ac11371fa72d4
ohjaeho <ovsoce@gmail.com>
2017-09-15 18:14:18 +0900

    WIP - Settings.

--- app/src/main/java/io/github/ovso/righttoknow/settings/SettingsFragment.java
+++ app/src/main/java/io/github/ovso/righttoknow/settings/SettingsFragment.java
@@ -1,9 +1,13 @@
 package io.github.ovso.righttoknow.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.annotation.XmlRes;
+import android.support.v4.app.NotificationManagerCompat;
 
 /**
  * Created by jaeho on 2017. 9. 15
@@ -16,9 +20,22 @@ public class SettingsFragment extends PreferenceFragment implements SettingsFrag
     super.onCreate(savedInstanceState);
     presenter = new SettingsFragmentPresenterImpl(this);
     presenter.onCreate(savedInstanceState);
+    boolean enable = NotificationManagerCompat.from(getActivity().getApplicationContext()).areNotificationsEnabled();
   }
 
   @Override public void setContentView(@XmlRes int resId) {
     addPreferencesFromResource(resId);
+
+    Preference preference = findPreference("notifications_key");
+    preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+      @Override public boolean onPreferenceClick(Preference preference) {
+        //Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        //Intent intent = new Intent(Settings.ACTION_APPLICATION_SETTINGS);
+
+        Intent intent = new Intent(Settings.ACTION_MANAGE_ALL_APPLICATIONS_SETTINGS);
+        startActivity(intent);
+        return false;
+      }
+    });
   }
 }
