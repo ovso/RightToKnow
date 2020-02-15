31d388ec6d576d72098a5f8d9774bc40c83b8c8a
jaeho <ovsoce@gmail.com>
2020-02-15 19:39:24 +0900

    Remote AdsActivity2, Edit Toolbar login, Replace VideoActivity theme/activity

--- app/src/main/java/io/github/ovso/righttoknow/framework/BaseActivity.java
+++ app/src/main/java/io/github/ovso/righttoknow/framework/BaseActivity.java
@@ -7,7 +7,6 @@ import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import dagger.android.support.DaggerAppCompatActivity;
@@ -16,21 +15,26 @@ import io.github.ovso.righttoknow.framework.ad.MyAdView;
 
 public abstract class BaseActivity extends DaggerAppCompatActivity {
 
-  protected @BindView(R.id.toolbar) Toolbar toolbar;
-  protected @BindView(R.id.ad_container) ViewGroup adContainer;
   private Unbinder unbinder;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(getLayoutResId());
     unbinder = ButterKnife.bind(this);
-    setSupportActionBar(toolbar);
+    final Toolbar toolbar = findViewById(R.id.toolbar);
+    if (toolbar != null) {
+      setSupportActionBar(toolbar);
+    }
+
     setNavigationBarColor();
     showAd();
   }
 
   protected void showAd() {
-    adContainer.addView(MyAdView.getAdmobAdView(getApplicationContext()));
+    ViewGroup adContainer = findViewById(R.id.ad_container);
+    if (adContainer != null) {
+      adContainer.addView(MyAdView.getAdmobAdView(getApplicationContext()));
+    }
   }
 
   private void setNavigationBarColor() {
@@ -47,6 +51,6 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
   }
 
   protected Toolbar getToolbar() {
-    return toolbar;
+    return findViewById(R.id.toolbar);
   }
 }
--- app/src/main/java/io/github/ovso/righttoknow/ui/video/PortraitVideoActivity.java
+++ app/src/main/java/io/github/ovso/righttoknow/ui/video/PortraitVideoActivity.java
@@ -3,20 +3,17 @@ package io.github.ovso.righttoknow.ui.video;
 import android.os.Bundle;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
-import butterknife.ButterKnife;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 import io.github.ovso.righttoknow.R;
 import io.github.ovso.righttoknow.Security;
-import io.github.ovso.righttoknow.framework.AdsActivity2;
+import io.github.ovso.righttoknow.framework.AdsActivity;
 
-public class PortraitVideoActivity extends AdsActivity2 {
+public class PortraitVideoActivity extends AdsActivity {
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_fullscreen_video);
-    ButterKnife.bind(this);
     if (getIntent().hasExtra("video_id")) {
       YouTubePlayerFragment youTubePlayerFragment =
           (YouTubePlayerFragment) getFragmentManager().findFragmentById(R.id.youtube_fragment);
@@ -35,5 +32,9 @@ public class PortraitVideoActivity extends AdsActivity2 {
       Toast.makeText(this, R.string.no_videos_found, Toast.LENGTH_SHORT).show();
       finish();
     }
+  }
+
+  @Override protected int getLayoutResId() {
+    return R.layout.activity_fullscreen_video;
   }
 }
