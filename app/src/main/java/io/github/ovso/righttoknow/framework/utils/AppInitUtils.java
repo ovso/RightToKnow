0e088adf454410b5b31ec21d5f8892d29ddb9e85
jaeho <ovsoce@gmail.com>
2020-06-27 21:22:30 +0900

    Fixed io.reactivex.exceptions.UndeliverableException

--- app/src/main/java/io/github/ovso/righttoknow/framework/utils/AppInitUtils.java
+++ app/src/main/java/io/github/ovso/righttoknow/framework/utils/AppInitUtils.java
@@ -1,15 +1,21 @@
 package io.github.ovso.righttoknow.framework.utils;
 
 import android.content.Context;
+
 import com.downloader.PRDownloader;
 import com.google.android.gms.ads.MobileAds;
+
+import java.io.IOException;
+import java.net.SocketException;
+
 import io.github.ovso.righttoknow.Security;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 import timber.log.Timber;
 
 public final class AppInitUtils {
 
-  private AppInitUtils() {
-  }
+  private AppInitUtils() {}
 
   public static void ad(Context context) {
     MobileAds.initialize(context, Security.ADMOB_APP_ID.getValue());
@@ -28,5 +34,42 @@ public final class AppInitUtils {
 
   public static void prDownloader(Context context) {
     PRDownloader.initialize(context);
+  }
+
+  //noinspection UnnecessaryParentheses
+  public static void rxJavaPlugin() {
+    RxJavaPlugins.setErrorHandler(
+        e -> {
+          if (e instanceof UndeliverableException) {
+            e = e.getCause();
+          }
+          if ((e instanceof IOException) || (e instanceof SocketException)) {
+            // fine, irrelevant network problem or API that throws on cancellation
+            return;
+          }
+          if (e instanceof InterruptedException) {
+            // fine, some blocking code was interrupted by a dispose call
+            return;
+          }
+          if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
+            // that's likely a bug in the application
+            Thread.UncaughtExceptionHandler uncaughtExceptionHandler =
+                Thread.currentThread().getUncaughtExceptionHandler();
+            if (uncaughtExceptionHandler != null) {
+              uncaughtExceptionHandler.uncaughtException(Thread.currentThread(), e);
+            }
+            return;
+          }
+          if (e instanceof IllegalStateException) {
+            // that's a bug in RxJava or in a custom operator
+            Thread.UncaughtExceptionHandler uncaughtExceptionHandler =
+                Thread.currentThread().getUncaughtExceptionHandler();
+            if (uncaughtExceptionHandler != null) {
+              uncaughtExceptionHandler.uncaughtException(Thread.currentThread(), e);
+            }
+            return;
+          }
+          Timber.tag("Undeliverable exception").w(e);
+        });
   }
 }
